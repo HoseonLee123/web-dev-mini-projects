1f8a637fe45067c606007e399ac67958a75525fd
HoseonLee <hoseon310@gmail.com>
2025-11-26 00:04:28 +0900

    Added a new project.

---
+++ text-highlighting-links/README.md
@@ -0,0 +1,3 @@
+# A Ref
+
++ [Text fragments](https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Fragment/Text_fragments)
---
+++ text-highlighting-links/src/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    window.location.href = "/";
+});
