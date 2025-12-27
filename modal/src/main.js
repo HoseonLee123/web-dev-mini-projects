1f2f44e008fc017c93c65fa033eee8c6bab6c801
HoseonLee <hoseon310@gmail.com>
2025-12-27 16:54:51 +0900

    Modified some code. Used semantic elements. Used the smoothly showing and hiding technique.

--- modal/src/main.js
+++ modal/src/main.js
@@ -4,15 +4,14 @@ const showModal = document.querySelector(".show-modal");
 const modalContainer = document.querySelector(".modal-container");
 const close = document.querySelector(".close");
 
-showModal.addEventListener("click", () => {
-    modalContainer.classList.add("display");
+setTimeout(() => {
+    modalContainer.style.transition = "all 0.5s allow-discrete";
+});
 
-    setTimeout(() => {
-        modalContainer.classList.add("opacity");
-    }, 100);
+showModal.addEventListener("click", () => {
+    modalContainer.classList.add("toggle");
 });
 
 close.addEventListener("click", () => {
-    modalContainer.classList.remove("display");
-    modalContainer.classList.remove("opacity");
+    modalContainer.classList.remove("toggle");
 });
